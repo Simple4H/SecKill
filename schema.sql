2ea0747fdc30477f839c072e6459a2a9f730c797
simple <crescentcxm@gmail.com>
2018-10-16 21:51:42 +0800

    修改相关配置 修改ignore

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+drop database db_seckill;
 CREATE DATABASE db_seckill;
 
 -- 创建数据库
@@ -11,9 +12,9 @@ CREATE TABLE seckill (
   COMMENT '商品名称',
   `number`      INT          NOT NULL
   COMMENT '库存数量',
-  `start_time`  TIMESTAMP    NOT NULL
+  `start_time`  datetime    NOT NULL
   COMMENT '秒杀开始时间',
-  `end_time`    TIMESTAMP    NOT NULL
+  `end_time`   datetime    NOT NULL
   COMMENT '秒杀结束时间',
   `create_time` TIMESTAMP    NOT NULL DEFAULT current_timestamp
   COMMENT '创建时间',
@@ -32,10 +33,10 @@ CREATE TABLE seckill (
 
 INSERT INTO seckill (NAME, NUMBER, start_time, end_time
 ) VALUES
-  ('抢1元红包', 1, '2017-11-02 00:00:00', '2017-11-03 00:00:00'),
-  ('抢10元红包', 10, '2017-11-02 00:00:00', '2017-11-03 00:00:00'),
-  ('抢100元红包', 100, '2017-11-02 00:00:00', '2017-11-03 00:00:00'),
-  ('抢1000元红包', 1000, '2017-11-02 00:00:00', '2017-11-03 00:00:00');
+  ('抢1元红包', 1, '2018-11-02 00:00:00', '2018-11-03 00:00:00'),
+  ('抢10元红包', 10, '2018-11-02 00:00:00', '2018-11-03 00:00:00'),
+  ('抢100元红包', 100, '2018-11-02 00:00:00', '2018-11-03 00:00:00'),
+  ('抢1000元红包', 1000, '2018-11-02 00:00:00', '2018-11-03 00:00:00');
 
 -- 秒杀成功表
 -- 用户登陆认证有有关的信息
@@ -47,7 +48,7 @@ CREATE TABLE success_killed (
   COMMENT '用户手机号',
   `state`       TINYINT   NOT NULL DEFAULT -1
   COMMENT '状态提示:-1:无效, 0:成功 1:已付款 2:已发货',
-  `create_time` TIMESTAMP NOT NULL
+  `create_time` datetime NOT NULL
   COMMENT '创建时间',
   PRIMARY KEY (seckill_id, user_phone), /*联合主键*/
   key idx_create_time(create_time)
